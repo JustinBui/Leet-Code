1ada0523a56bfc22a12c878d73ac98f8121fc980
BuiTheBoi <uNUKEcorn15@gmail.com>
2021-07-28 17:07:34 -0700

    Completed a problem on reorder list :airplane:

---
+++ Arrays/searchInRotatedSorted.cpp
@@ -0,0 +1,18 @@
+// Source: https://leetcode.com/problems/search-in-rotated-sorted-array/
+
+#include <iostream>
+#include <vector>
+
+class Solution
+{
+public:
+    int search(vector<int> &nums, int target)
+    {
+    }
+};
+
+int main()
+{
+
+    return 0;
+}
---
+++ Singly Linked List/reorderList.cpp
@@ -0,0 +1,64 @@
+// Source: https://leetcode.com/problems/reorder-list/
+// Solution: https://www.youtube.com/watch?v=S5bfdUTrKLM (Not like the code below)
+
+#include <iostream>
+#include <vector>
+#include "SLL.hpp"
+using namespace std;
+
+// THIS IS MY OWN SOLUTION!!
+class Solution
+{
+public:
+    void reorderList(ListNode *head)
+    {
+        if (head->next != nullptr) // More than one head
+        {
+            ListNode *placeHolder = head;
+            ListNode *tail = head;
+            int loops = 0;
+
+            while (placeHolder->next != nullptr)
+            {
+                // Traversing tail to last node
+                while (tail->next != nullptr)
+                    tail = tail->next;
+
+                if (loops % 2 == 0)
+                { // Makes sure this happens every 2 iterations
+                    tail->next = placeHolder->next;
+                    placeHolder->next = tail;
+
+                    // Second to last serves as the next 'tail'
+                    ListNode *secondToLast = tail->next;
+                    while (secondToLast->next != tail)
+                        secondToLast = secondToLast->next;
+                    secondToLast->next = nullptr;
+                }
+                placeHolder = placeHolder->next;
+                loops++;
+            }
+        }
+    }
+};
+
+int main()
+{
+    Solution sol;
+    IntLinkedList linked;
+
+    vector<int> nums = {1, 2, 3, 4};
+    for (vector<int>::iterator it = nums.begin(); it != nums.end(); ++it)
+    {
+        linked.addToEnd(*it);
+    }
+
+    cout << "Before: ";
+    linked.printAllElements();
+
+    sol.reorderList(linked.getHead());
+    cout << "After: ";
+    linked.printAllElements();
+
+    return 0;
+}
