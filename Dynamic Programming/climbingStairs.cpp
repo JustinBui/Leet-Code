7f4a939eff6818726ec59871755d1fc4a9c85cb2
BuiTheBoi <uNUKEcorn15@gmail.com>
2021-07-31 20:17:16 -0700

    Starting to do Dynamic Programming examples :hospital: :skull:

---
+++ Dynamic Programming/climbingStairs.cpp
@@ -0,0 +1,25 @@
+// Soluion: https://www.youtube.com/watch?v=Y0lT9Fck7qI
+#include <iostream>
+
+int climbStairs(int);
+
+int main()
+{
+
+    return 0;
+}
+
+// BOTTOM UP APPROACH
+int climbStairs(int n)
+{
+    int oneStep = 1, twoSteps = 1;
+
+    for (int i = 0; i < n - 1; ++i)
+    {
+        int temp = oneStep;
+        oneStep = temp + twoSteps;
+        twoSteps = temp;
+    }
+
+    return oneStep;
+}
