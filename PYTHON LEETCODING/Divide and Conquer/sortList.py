d5a2cd403dee98210a4f0b0da7e2aef393139d90
BuiTheBoi <uNUKEcorn15@gmail.com>
2021-12-20 23:45:43 -0800

    Merge Sort Problem done :car: :hibiscus:

---
+++ PYTHON LEETCODING/Divide and Conquer/sortList.py
@@ -0,0 +1,43 @@
+class Solution:
+    def sortArray(self, nums: list[int]) -> list[int]:
+        if (len(nums) == 1):    # BASE CASE
+            return nums
+
+        else:   # RECURSIVE CASE
+            # Using divide and conquer
+            mid = (len(nums) - 1) // 2
+            left = self.sortArray(nums[:mid + 1])
+            right = self.sortArray(nums[mid + 1:])
+
+            # i is index of left subarray
+            # j is index of right subarray
+            i = j = 0
+            newArr = []
+
+            # Merging left and right subarrays
+            while (i < len(left) and j < len(right)):
+                if (left[i] < right[j]):
+                    newArr.append(left[i])
+                    i += 1
+                else:  # if (right[j] <= left[i]):
+                    newArr.append(right[j])
+                    j += 1
+
+            # Filling in the remaining numbers either from left or right
+            while (i < len(left)):
+                newArr.append(left[i])
+                i += 1
+            while (j < len(right)):
+                newArr.append(right[j])
+                j += 1
+
+            return newArr
+
+
+if (__name__ == "__main__"):
+    mySol = Solution()
+
+    arr = [49, 54, 23, 2, 2, 2, 45, 0, -33, -2]
+    result = mySol.sortArray(arr)
+
+    print(result)
