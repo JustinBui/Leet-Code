d925f902b1929bdac51ee758e82c7b8368257cdd
BuiTheBoi <uNUKEcorn15@gmail.com>
2022-01-19 18:56:59 -0800

    Longest Substring without repeating characters :bullettrain_side: :airplane: :city_sunset:

--- PYTHON LEETCODING/Strings/longestSubstrWithoutRepeats.py
+++ PYTHON LEETCODING/Strings/longestSubstrWithoutRepeats.py
@@ -2,6 +2,27 @@ class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         longest_length = 0
 
-        for char in s:
+        starting_point = 0
+        visited = set()
 
+        while (starting_point < len(s)):
+
+            for i in range(starting_point, len(s)):
+                if (s[i] not in visited):
+                    visited.add(s[i])
+
+                else:
+                    starting_point += 1
+                    if (len(visited) > longest_length):
+                        longest_length = len(visited)
+                    visited.clear()
+
+                    break
         return longest_length
+
+
+test = Solution()
+s = "jbpnbwwd"
+
+length = test.lengthOfLongestSubstring(s)
+print(length)
