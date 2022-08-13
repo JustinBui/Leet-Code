9b0905f37e939672c2ce78b801d3eb2135842f62
BuiTheBoi <uNUKEcorn15@gmail.com>
2022-08-13 16:57:47 -0700

    Made a more efficient solution: 59 ms, 16.5 MB

--- NEETCODE/Trees/validateBST.py
+++ NEETCODE/Trees/validateBST.py
@@ -1,19 +1,30 @@
 class Solution:
-    def check(self, root: Optional[TreeNode], choice: str, val_parent: int) -> bool:
-        if not root:
-            return True
-        if choice == 'l' and root.val >= val_parent:
-            return False
-        elif choice == 'r' and root.val <= val_parent:
-            return False
+    # def check(self, root: Optional[TreeNode], choice: str, val_parent: int) -> bool:
+    #     if not root:
+    #         return True
+    #     if choice == 'l' and root.val >= val_parent:
+    #         return False
+    #     elif choice == 'r' and root.val <= val_parent:
+    #         return False
 
-        return self.check(root.left, choice, val_parent) and self.check(root.right, choice, val_parent)
+    #     return self.check(root.left, choice, val_parent) and self.check(root.right, choice, val_parent)
+
+    # def isValidBST(self, root: Optional[TreeNode]) -> bool:
+    #     if not root:
+    #         return True
+    #     if self.check(root.left, 'l', root.val) == False or self.check(root.right, 'r', root.val) == False:
+    #         return False
+
+    #     # If the current node at root is true, keep searching down the tree to see if there are any false
+    #     return self.isValidBST(root.left) and self.isValidBST(root.right)
 
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        if not root:
-            return True
-        if self.check(root.left, 'l', root.val) == False or self.check(root.right, 'r', root.val) == False:
-            return False
+        def validate(root: Optional[TreeNode], left: int, right: int) -> bool:
+            if not root:
+                return True
+            if not (left < root.val and root.val < right):
+                return False
 
-        # If the current node at root is true, keep searching down the tree to see if there are any false
-        return self.isValidBST(root.left) and self.isValidBST(root.right)
+            return validate(root.left, left, root.val) and validate(root.right, root.val, right)
+
+        return validate(root, float('-inf'), float('inf'))
