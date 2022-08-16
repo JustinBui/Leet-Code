76a280e70126d0c3a0dcd482880774fe3c3e002d
BuiTheBoi <uNUKEcorn15@gmail.com>
2022-08-16 16:23:54 -0700

    17. Letter Combinations of a Phone Number: 44 ms, 13.9 MB

--- NEETCODE/Backtrack/letterComboOfPhoneNum.py
+++ NEETCODE/Backtrack/letterComboOfPhoneNum.py
@@ -1,18 +1,32 @@
 class Solution:
-    def letterCombinations(self, digits: str) -> List[str]:
+    def letterCombinations(self, digits: str) -> list[str]:
         res = []
-        letters = ''
+        letters = []
         legend = {'2': 'abc', '3': 'def', '4': 'ghi', '5': 'jkl',
                   '6': 'mno', '7': 'pqrs', '8': 'tuv', '9': 'wxyz'}
 
+        if len(digits) == 0:
+            return res
+
         def backtrack(i):
             if i == len(digits):
-                res.append(letters[:])
+                res.append(''.join(letters))
                 return
 
-            for j in range(len(legend[digits[i]])):
-                letters.append(legend[digits[i]])
+            # for j in range(i, len(digits)):
+            for l in legend[digits[i]]:
+                letters.append(l)
                 backtrack(i + 1)
                 letters.pop()
 
+        backtrack(0)
+
         return res
+
+
+if __name__ == '__main__':
+    digits = "275"
+
+    obj = Solution()
+
+    print(obj.letterCombinations(digits))
