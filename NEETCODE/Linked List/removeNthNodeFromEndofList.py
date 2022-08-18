d948e524324e7e09276b37fca1c66d5be499a592
BuiTheBoi <uNUKEcorn15@gmail.com>
2022-08-18 16:21:45 -0700

    LeetCode 19 Optimized: 42 ms, 13.9 MB

--- NEETCODE/Linked List/removeNthNodeFromEndofList.py
+++ NEETCODE/Linked List/removeNthNodeFromEndofList.py
@@ -3,40 +3,60 @@ from typing import Optional
 
 
 class Solution:
-    def invert(self, head):
-        # Invert Linked List
-        pre = None
-        post = head
+    # ------------------------------ Approach By Inverting Linked List ------------------------------
+    # def invert(self, head):
+    #     # Invert Linked List
+    #     pre = None
+    #     post = head
 
-        while head:
-            post = head.next
-            head.next = pre
-            pre = head
-            head = post
-        head = pre
+    #     while head:
+    #         post = head.next
+    #         head.next = pre
+    #         pre = head
+    #         head = post
+    #     head = pre
 
-        return head
+    #     return head
 
+    # def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+    #     head = self.invert(head)
+
+    #     i = 1
+    #     it = head
+    #     while i < n:
+    #         it = it.next
+    #         i += 1
+
+    #     # Delete first element of the list
+    #     if it == head:
+    #         head = head.next
+    #     else:  # Deleting any element from list that is not head
+    #         pre_it = head
+    #         while pre_it.next != it:
+    #             pre_it = pre_it.next
+    #         pre_it.next = pre_it.next.next
+    #     it.next = None
+
+    #     return self.invert(head)
+
+    # ---------------------------- Optimized Using 2 Pointer Approach ----------------------------
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        head = self.invert(head)
+        dummy = ListNode(0, next=head)
 
-        i = 1
-        it = head
-        while i < n:
-            it = it.next
-            i += 1
+        left = dummy
+        right = head
 
-        # Delete first element of the list
-        if it == head:
-            head = head.next
-        else:  # Deleting any element from list that is not head
-            pre_it = head
-            while pre_it.next != it:
-                pre_it = pre_it.next
-            pre_it.next = pre_it.next.next
-        it.next = None
+        while n > 0:
+            right = right.next
+            n -= 1
 
-        return self.invert(head)
+        while right:
+            left = left.next
+            right = right.next
+
+        left.next = left.next.next
+
+        return dummy.next
 
 
 if __name__ == "__main__":
