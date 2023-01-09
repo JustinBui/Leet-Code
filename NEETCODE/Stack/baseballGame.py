e60041bd19c3564690ba41830a9bde969db2bc7b
Justin Bui <uNUKEcorn15@gmail.com>
2023-01-08 18:41:37 -0800

    682. Baseball Game: 48 ms, 14 MB

---
+++ NEETCODE/Stack/baseballGame.py
@@ -0,0 +1,15 @@
+class Solution:
+    def calPoints(self, operations: List[str]) -> int:
+        res =[]
+
+        for o in operations:
+            if o == '+':
+                res.append(res[-1] + res[-2])
+            elif o == 'D':
+                res.append(res[-1] * 2)
+            elif o == 'C':
+                res.pop()
+            else: # A string representing a number
+                res.append(int(o))
+
+        return sum(res)
